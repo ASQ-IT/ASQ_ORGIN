1ef613b5cdecb2b2aaec2f5afe5a482f4c06e9a9
aftab <aftab@asalqurashi.com>
2024-07-18 09:37:15 +0300

    [ASQ][Atul][Commit the changes]

---
+++ asq_pos/src/asq/pos/loyalty/stc/tender/service/IAsqSTCLoyaltyServiceRequest.java
@@ -0,0 +1,7 @@
+package asq.pos.loyalty.stc.tender.service;
+
+import dtv.service.req.IServiceRequest;
+
+public interface IAsqSTCLoyaltyServiceRequest extends IServiceRequest {
+
+}
---
+++ asq_pos/src/asq/pos/loyalty/stc/tender/service/IAsqSTCLoyaltyTenderService.java
@@ -0,0 +1,20 @@
+package asq.pos.loyalty.stc.tender.service;
+
+import dtv.service.IService;
+import dtv.service.ServiceException;
+import dtv.service.req.IServiceResponse;
+
+public interface IAsqSTCLoyaltyTenderService extends IService {
+	
+	IServiceResponse submitOTPRequest(IAsqSTCLoyaltyServiceRequest paramAsqSTCLoyalityRequest)throws ServiceException;
+
+	/*
+	 * IServiceResponse submitRedeemRequest(AsqSTCLoyaltyServiceRequest
+	 * paramAsqSTCLoyalityRequest)throws ServiceException;
+	 * 
+	 * IServiceResponse submitReverseRedeemRequest(AsqSTCLoyaltyServiceRequest
+	 * paramAsqSTCLoyalityRequest)throws ServiceException;
+	 */
+
+
+}
